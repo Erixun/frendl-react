4b7ef78c31666a31257024f63f35d22b1369a0f4
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-15 20:07:37 +0200

    use cors w options

--- src/api/src/app.ts
+++ src/api/src/app.ts
@@ -1,14 +1,17 @@
-// import { Express } from "express";
-
 import express, { json } from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
-app.use(json());
-app.use('/api', routes);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Frendl!');
-// });
+//TODO: Move to separate file in config folder
+const corsOptions = {
+  origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+};
+
+app.use(json());
+app.use(cors(corsOptions));
+app.use('/api', routes);
 
 export default app;
