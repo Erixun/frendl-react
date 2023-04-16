9fc12d10006aee50e23c7cf530048ca2d8ee1238
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-16 17:34:54 +0200

    cleanup

--- src/api/src/app.ts
+++ src/api/src/app.ts
@@ -1,17 +1,10 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import Pusher from 'pusher';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
-let pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID || '',
-  key: process.env.PUSHER_APP_KEY || '',
-  secret: process.env.PUSHER_APP_SECRET || '',
-  cluster: process.env.PUSHER_APP_CLUSTER || '',
-  useTLS: true,
-});
 
 //TODO: Move to separate file in config folder
 const corsOptions = {
