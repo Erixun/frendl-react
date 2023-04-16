ba98880bf0227403ec66420cb04c7daf9272984a
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-16 17:33:53 +0200

    make pusher file to instantiate pusher

---
+++ src/api/src/service/pusher.ts
@@ -0,0 +1,13 @@
+import Pusher from 'pusher';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID || '',
+  key: process.env.PUSHER_APP_KEY || '',
+  secret: process.env.PUSHER_APP_SECRET || '',
+  cluster: process.env.PUSHER_APP_CLUSTER || '',
+  useTLS: true,
+});
+
+export default pusher;
