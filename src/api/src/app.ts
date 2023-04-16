6c90f94524dca270993af2912928943767f9eb04
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-16 17:16:53 +0200

    import & instantiate pusher

--- src/api/src/app.ts
+++ src/api/src/app.ts
@@ -1,8 +1,17 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import Pusher from 'pusher';
 
 const app = express();
+
+let pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID || '',
+  key: process.env.PUSHER_APP_KEY || '',
+  secret: process.env.PUSHER_APP_SECRET || '',
+  cluster: process.env.PUSHER_APP_CLUSTER || '',
+  useTLS: true,
+});
 
 //TODO: Move to separate file in config folder
 const corsOptions = {
