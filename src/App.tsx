54ab02125c080541a7e9def917e9a61c0ffb2407
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-30 20:59:00 +0200

    use currentUser

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import { observer } from 'mobx-react-lite';
 import DrawerWelcome from './components/DrawerWelcome';
 import { useDisclosure } from '@chakra-ui/react';
 import GridMapOverlay from './components/GridMapOverlay';
+import { currentUser } from './testData';
 import './App.css';
 
-const mapStore = new MapStore();
+const mapStore = new MapStore(currentUser);
 mapStore.findMyLocation();
 const App = observer(() => {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
 
-return (
+  return (
     <div className="App">
-      <DrawerWelcome mapStore={mapStore} isOpen={isOpen} onClose={onClose} />
+      <DrawerWelcome map={mapStore} isOpen={isOpen} onClose={onClose} />
       {/* TODO: Create ViewZone component? */}
       <div id="map" className="map"></div>
-      <GridMapOverlay mapStore={mapStore} onOpenDrawer={onOpen} />
+      {!isOpen && <GridMapOverlay map={mapStore} onOpenDrawer={onOpen} />}
     </div>
   );
 });
