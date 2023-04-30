3d4ff250c19a46bd82ce3ce89b38503d3688fbe5
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-30 20:59:36 +0200

    simplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
 
 //AIzaSyB5rk666lWxXOOS9MyAJy2dj--Eis4C5KY
