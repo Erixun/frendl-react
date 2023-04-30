57d2fcf16d0c75b388be21eae47585d554d9ea78
Erik Sundberg <erik.sundberg@unicus.com>
2023-04-30 14:51:22 +0200

    create member test data

---
+++ src/testData.ts
@@ -0,0 +1,28 @@
+const members = [
+  {
+    username: 'Erik Emanuel',
+    status: 'online',
+    location: {
+      lat: 59.35,
+      lng: 18.04,
+    },
+  },
+  {
+    username: 'Melvin Moore',
+    status: 'offline',
+    location: {
+      lat: 59.36,
+      lng: 18.05,
+    },
+  },
+  {
+    username: 'Malva Melin',
+    status: 'online',
+    location: {
+      lat: 59.365,
+      lng: 18.07,
+    },
+  },
+];
+
+export { members };
