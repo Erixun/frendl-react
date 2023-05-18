dbce0ef6b1a080e5ccdc3b38f24ff5954e7eaaeb
Erik Emanuel <Erik.Emanuel.Sundberg@outlook.com>
2023-05-18 21:20:26 +0200

    useMapStore instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { MapStore } from './store/mapStore';
 import { observer } from 'mobx-react-lite';
 import DrawerWelcome from './components/DrawerWelcome';
 import { useDisclosure } from '@chakra-ui/react';
 import GridMapOverlay from './components/GridMapOverlay';
-import { currentUser } from './testData';
 import './App.css';
+import { useMapStore } from './hooks/useMapStore';
 
-const mapStore = new MapStore(currentUser);
+const mapStore = useMapStore()//new MapStore(currentUser);
 mapStore.findMyLocation();
 const App = observer(() => {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
