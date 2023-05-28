e60f9fd53762beb48834e6a206702871f495bd39
Erik Emanuel <Erik.Emanuel.Sundberg@outlook.com>
2023-05-28 20:22:19 +0200

    add util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,12 @@ import isLandscape from './isLandscape';
 import notify from './notify';
 import isValidZoneCode from './isValidZoneCode';
 import getRandomLocation from './getRandomLocation';
+import writeContent from './writeContent';
 
-export { isLandscape, notify, isValidZoneCode, getRandomLocation };
+export {
+  isLandscape,
+  notify,
+  isValidZoneCode,
+  getRandomLocation,
+  writeContent,
+};
